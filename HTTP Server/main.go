d71b41ab37e5065f1c3cf13f14f0a7e47c8ca4af
Tharanishwara <tharanishwaran.ta@gmail.com>
2024-07-10 10:44:20 +0530

    http server project added

---
+++ HTTP Server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Define a custom handler struct
+type APIHandler struct{}
+
+// Implement the ServeHTTP method on the APIHandler struct
+func (APIHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request:", r.Method, r.URL.Path)
+	// Write "hello world" to the response
+	fmt.Fprint(w, "hello world")
+	fmt.Fprint(w,"hello everyone")
+	fmt.Println("hi bro")
+}
+
+func main() {
+	// Start the HTTP server on port 8000 with APIHandler as the handler
+	log.Println("Starting server on :8000")
+	err := http.ListenAndServe(":8000", &APIHandler{})
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
